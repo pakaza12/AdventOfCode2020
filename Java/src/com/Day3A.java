3da21861224350ff4ef8af0897fae5f2d7b5d706
Parker Zach <pkzach123@gmail.com>
2020-12-18 23:55:42 -0600

    Solved 3a and 3b
    Use the rightShift and downShift ints at the top to change the right and down shifts to whatever you want. For 3b, mutliply your answers together

---
+++ Java/src/com/Day3A.java
@@ -0,0 +1,91 @@
+package com;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day3A {
+
+	public static void main(String[] args) {
+		
+		//Num rows = n
+		//num columns needed = (n-1)*3
+		//Num times to copy over = roundUp(num columns needed / numColumns)
+		
+		int rightShift = 1;
+		int downShift = 2;
+		
+		Scanner input = null;
+		try {
+			input = new Scanner(new File("Data/Day3Data.dat"));
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+		
+		int numColumns = 0;
+		int numRows = 0;
+		//Get the number of columns and rows in the inputFile
+		while(input.hasNextLine()) {
+			char[] a = input.nextLine().toCharArray();
+			numColumns = a.length;
+			numRows++;
+		}
+		input.close();
+		
+		try {
+			input = new Scanner(new File("Data/Day3Data.dat"));
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+		
+		
+		int numColumnsNeeded = (numRows-1) * rightShift;
+		int numCopy = (int) Math.ceil((double)numColumnsNeeded / (double)numColumns);
+		
+		int numTrees = 0;
+		int numNotTrees = 0;
+		char graph[][] = new char[numRows][numColumnsNeeded+1];
+		
+		int b = 0;
+		while(input.hasNextLine()) {
+			char a[] = input.nextLine().toCharArray();
+			
+			int c = 0;
+			for(int i = 0; i < numColumnsNeeded+1; i++) {
+				graph[b][i] = a[c];
+				c++;
+				if(c == a.length) {
+					c = 0;
+				}
+			}
+			b++;
+		}
+		
+		b = downShift;
+		for(int i = rightShift; i < numColumnsNeeded+1; i+=rightShift) {
+			if(b > numRows) {
+				break;
+			}
+			System.out.print(graph[b][i]);
+			if(graph[b][i] == '.') {
+				graph[b][i] = 'O';
+				numNotTrees++;
+			} else if(graph[b][i] == '#') {
+				graph[b][i] = 'X';
+				numTrees++;
+			}
+			b+=downShift;
+		}
+		
+		for(int i = 0; i < numRows; i++) {
+			for(int j = 0; j < numColumnsNeeded+1; j++) {
+				System.out.print(graph[i][j]);
+			}
+			System.out.println();
+		}
+		
+		System.out.println(numTrees + " " + numNotTrees);
+
+	}
+
+}
